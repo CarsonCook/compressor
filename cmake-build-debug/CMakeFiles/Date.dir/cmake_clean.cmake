af7e2562194a6dbd41b2480e23df4983b5989ff6
Carson Cook <cook.carson38@gmail.com>
2019-04-07 22:42:13 -0400

    init commit

---
+++ cmake-build-debug/CMakeFiles/Date.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Date.dir/main.cpp.obj"
+  "Date.pdb"
+  "Date.exe"
+  "Date.exe.manifest"
+  "libDate.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Date.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,4 @@
+//
+// Created by cookc on 2019-04-07.
+//
+
